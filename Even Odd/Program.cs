50b1edf5413f4800fcb633a78a29ca784bc9d6bc
Tyagi.Garvit <Tyagi.Garvit@INLPF2RX2LR>
2022-09-22 16:39:13 +0530

    Add project files.

---
+++ Even Odd/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Even_Odd
+{
+    public class evenodd
+    {
+
+        double number1 = Convert.ToDouble(Console.ReadLine());
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        public bool pgm()
+        {
+            if (number1 % 2 == 0 && number2 % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+            
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            evenodd obj = new evenodd();
+            Console.WriteLine(obj.pgm());
+                
+
+                
+        }
+    }
+}
